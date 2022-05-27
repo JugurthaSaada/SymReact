d2ef70bd0a51ff16ba9d04126be7769e1e193ff9
unknown <saadajugurtha59@gmail.com>
2022-05-27 15:47:09 +0200

    Modification sur le cycle de vie de la requête et extension de Doctrine

---
+++ src/Events/PasswordEncoderSubscriber.php
@@ -0,0 +1,39 @@
+<?php
+namespace App\Events;
+
+use Symfony\Component\HttpKernel\KernelEvents;
+use Symfony\Component\HttpKernel\Event\ViewEvent;
+use ApiPlatform\Core\EventListener\EventPriorities;
+use App\Entity\User;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
+class PasswordEncoderSubscriber implements EventSubscriberInterface{
+
+    /** 
+     * @var UserPasswordHasherInterface 
+     * */
+    private $encoder;
+
+    public function __construct(UserPasswordHasherInterface $encoder)
+    {
+        $this->encoder = $encoder;
+    }
+
+    public static function getSubscribedEvents()
+    {
+        return [
+            KernelEvents::VIEW => ['encodePassword', EventPriorities::PRE_WRITE]
+        ];
+    }
+
+    public function encodePassword(ViewEvent $event){
+        $user = $event->getControllerResult();
+        $method = $event->getRequest()->getMethod(); //POST, GET, PUT, ......
+        
+        if($user instanceof User && $method === "POST"){
+            $hash = $this->encoder->hashPassword($user, $user->getPassword());
+            $user->setPassword($hash);
+        }
+    }
+}
